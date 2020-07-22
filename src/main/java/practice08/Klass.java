83530813da1f29730c1827ecd1f9ad2cb8e26249
benjamin <529709737@qq.com>
2020-07-22 22:42:49 +0800

    practice08 [Benjamin] feature: finish the practice06

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,26 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
+
+    public String getDisplayName(){
+        return "Class " + this.number;
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,39 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private Klass klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String baseIntro = super.introduce() + " I am a Teacher.";
+        if (klass != null) {
+            return baseIntro + " I teach Class " + klass.getNumber() + ".";
+        }else {
+            return baseIntro + " I teach No Class.";
+        }
+    }
+
+    public String introduceWith(Student student){
+        String baseIntro = super.introduce() + " I am a Teacher.";
+
+        if (this.klass == student.getKlass()) {
+            return baseIntro + " I teach " + student.getName() +  ".";
+        }else {
+            return baseIntro + " I don't teach " + student.getName() + ".";
+        }
+    }
 }
