58e7e182d75bc48159a369b68a9b5533d1895528
benjamin <529709737@qq.com>
2020-07-22 22:38:53 +0800

    practice04 [Benjamin] feature: finish the practice04

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,25 @@
 package practice04;
 
 public class Person {
+    private String name;
+    private int age;
+    private String basicIntro;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        basicIntro = "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return basicIntro;
+    }
 }
