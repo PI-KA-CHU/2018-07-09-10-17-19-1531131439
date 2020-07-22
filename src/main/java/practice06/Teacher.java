55a5bfb26f3f74e4afe57d1c2f8d3cd9f02235ad
benjamin <529709737@qq.com>
2020-07-22 22:41:17 +0800

    practice06 [Benjamin] feature: finish the practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass = -1;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String baseIntro = super.introduce();
+        String careerIntro = "I am a Teacher.";
+        if (klass == -1) {
+            return baseIntro + " " +  careerIntro + " I teach No Class.";
+        }
+        return baseIntro + " " + careerIntro + " I teach Class " + klass + ".";
+    }
 }
